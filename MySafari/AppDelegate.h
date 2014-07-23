2a81baa528b0d16c4cc1bd4b5b013120af75866b
Chris Meats <cmeats@etccomputerland.com>
2014-07-23 13:20:53 -0500

    first commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by ETC ComputerLand on 7/23/14.
+//  Copyright (c) 2014 cmeats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySafari/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by ETC ComputerLand on 7/23/14.
+//  Copyright (c) 2014 cmeats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
